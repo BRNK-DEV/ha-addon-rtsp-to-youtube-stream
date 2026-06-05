e9ac1e0c095456c5d43dc5234326135bf1b8428c
Benny Renkens <benny.renkens@gmail.com>
2026-06-05 19:15:07 +0200

    Improve run.sh with better error checking and logging

--- rtsp_to_youtube/run.sh
+++ rtsp_to_youtube/run.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/with-contenv bashio
 
-RTSP_URL=$(bashio::config 'rtsp_url')
-YOUTUBE_KEY=$(bashio::config 'youtube_key')
+set -e
+
+RTSP_URL=$(bashio::config 'rtsp_url' || echo "")
+YOUTUBE_KEY=$(bashio::config 'youtube_key' || echo "")
+
+# Validate configuration
+if [ -z "$RTSP_URL" ]; then
+  bashio::log.error "RTSP URL not configured. Please set rtsp_url in add-on configuration."
+  exit 1
+fi
+
+if [ -z "$YOUTUBE_KEY" ]; then
+  bashio::log.error "YouTube Stream Key not configured. Please set youtube_key in add-on configuration."
+  exit 1
+fi
 
 bashio::log.info "Starting RTSP -> YouTube stream"
 bashio::log.info "RTSP URL: ${RTSP_URL}"
 
 while true; do
+  bashio::log.info "Connecting to RTSP camera..."
   ffmpeg \
     -rtsp_transport tcp \
     -use_wallclock_as_timestamps 1 \
@@ -23,6 +37,6 @@ while true; do
     -f flv \
     "rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_KEY}"
 
-  bashio::log.warning "ffmpeg exited, restarting in 10 seconds"
+  bashio::log.warning "ffmpeg exited with code $?, restarting in 10 seconds"
   sleep 10
 done
